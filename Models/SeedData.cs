e9efaf2c34204c40d7bcfa855e7ac9d9417defcc
farrert <farrert@mail.nmc.edu>
2021-09-26 14:50:14 -0400

    SeedData Change

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using FrisbeeGolfCourseMap.Data;
-using System;
-using System.Linq;
+
 
 namespace FrisbeeGolfCourseMap.Models
 {
