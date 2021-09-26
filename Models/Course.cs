46df0be7be86fc7f5c353699ded5e9a7db734bc4
rphinizy <70117533+rphinizy@users.noreply.github.com>
2021-09-26 18:17:06 -0400

    Starting Fresh Manual Upload
    oh boy...

--- Models/Course.cs
+++ Models/Course.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 
 namespace FrisbeeGolfCourseMap.Models
 {
